abfb9fed320caa04437da52548cf6cf22fcbd483
Anuj Krishna Gupta <krishnagupta.6264@gmail.com>
2023-05-29 08:57:31 +0530

    Changed

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 module.exports = async  ()=>{
-    const dataBaseURL = `mongodb+srv://krishnagupta6264:VoRBKXTBOj83RJGa@cluster0.yiwz21q.mongodb.net/?retryWrites=true&w=majority`;
+    const dataBaseURL = process.env.MongoDB;
       try {
         const connect= await mongoose.connect(dataBaseURL, {
             useNewUrlParser: true, 
